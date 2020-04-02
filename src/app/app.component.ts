5698f291a58107d47e32f9086169e055632febd9
Tom Steele <tsteele@madisoncollege.edu>
2020-04-02 13:55:18 -0500

    Got service hooked up and working'ish...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
 @Component({
@@ -6,16 +6,18 @@ import { QuizService } from './quiz.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
   bg = Math.random() > 0.5 ? 'green' : 'yellow';
   borderRadius = Math.random() > 0.5 ? '30px' : '0px';
   toolTip = `This is ${this.bg} and has a border radius of ${this.borderRadius}`;
 
   // Need a ctor for DI of the QuizService.
-  constructor(private quizSvc: QuizService) {
-    console.log('I am here...')
-    console.log(this.quizSvc.loadQuizzes());
-  }
+  constructor(private quizSvc: QuizService) { }
 
+  quizzes = [];
+
+  ngOnInit() {
+    this.quizzes = this.quizSvc.loadQuizzes();
+  }
 }

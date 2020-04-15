b38b9de60fdcbb5c14ba97ec7f3b8f300399b33d
Tim <37000135+timzschernitz@users.noreply.github.com>
2020-04-15 17:51:12 -0500

    DI'd the HttpClient into QuizService

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private httpSvc: HttpClient) { }
 
   loadQuizzes() {
     const quizzesFromWeb = [
